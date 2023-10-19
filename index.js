fcf988a4e73acfec081bd6105593a2fa20247207
ikmat-rabib <ikmat.rbb@gmail.com>
2023-10-20 00:22:07 +0600

    created POST operation and collection in DB

--- index.js
+++ index.js
@@ -24,6 +24,19 @@ async function run() {
     try {
         // Connect the client to the server	(optional starting in v4.7)
         await client.connect();
+
+        const productCollection = client.db('productDb').collection('product')
+
+        app.post('/product', async(req,res) => {
+            const newProduct = req.body;
+            console.log(newProduct);
+        })
+
+
+
+
+
+
         // Send a ping to confirm a successful connection
         await client.db("admin").command({ ping: 1 });
         console.log("Pinged your deployment. You successfully connected to MongoDB!");
